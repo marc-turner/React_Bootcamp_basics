742fa3c6611a661171554ccd1729030baed9b6a8
Marc Turner <marc.t1967@gmail.com>
2020-01-16 22:43:11 -0500

    add first component

---
+++ first_component/index.js
@@ -0,0 +1,28 @@
+// class Hello extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello there!</h1>
+//                 <h1>Hello there!</h1>
+//                 <h1>Hello there!</h1>
+//             </div>
+//         );
+//     }
+// }
+
+// ReactDOM.render(<Hello />, document.getElementById('root'));
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <h1>Hello</h1>
+                <h1>Hello</h1>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
