5209bb45d97480a6ea50685c863d01c84a093b9f
Marc Turner <marc.t1967@gmail.com>
2020-01-25 10:44:36 -0500

    add conditionals 2

--- 03_JSX_Conditionals/index.js
+++ 03_JSX_Conditionals/index.js
@@ -1,26 +1,46 @@
+// function getNum() {
+// 	return Math.floor(Math.random() * 10) + 1;
+// }
+// class NumPicker extends React.Component {
+// 	render() {
+// 		const num = getNum();
+// 		let msg;
+// 		if (num === 7) {
+// 			msg =
+// 				<div>
+// 					<h2>CONGRATS YOU WIN!</h2>
+// 					<img src="https://i.giphy.com/media/nXxOjZrbnbRxS/giphy.webp" />
+// 				</div>
+// 		} else {
+// 			msg = <p>Sorry You Lose!</p>
+// 		}
+// 		return (
+// 			<div>
+// 				<h1>Your number is: {num} </h1>
+// 				{msg}
+// 			</div>
+// 		);
+// 	}
+// }
+
+// ReactDOM.render(<NumPicker />, document.getElementById('root'));
+
 function getNum() {
-	return Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
 }
+
 class NumPicker extends React.Component {
-	render() {
-		const num = getNum();
-		let msg;
-		if (num === 7) {
-			msg =
-				<div>
-					<h2>CONGRATS YOU WIN!</h2>
-					<img src="https://i.giphy.com/media/nXxOjZrbnbRxS/giphy.webp" />
-				</div>
-		} else {
-			msg = <p>Sorry You Lose!</p>
-		}
-		return (
-			<div>
-				<h1>Your number is: {num} </h1>
-				{msg}
-			</div>
-		);
-	}
+    render() {
+        const num = getNum();
+        // let msg;
+
+        return (
+            <div>
+                <h1>Your Number is...{num}</h1>
+                <p>{num === 7 ? 'congrats' : 'not lucky'}</p>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<NumPicker />, document.getElementById('root'));
