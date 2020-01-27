161f274c978f7431e11b6eee200bc21b568f1a88
Marc Turner <marc.t1967@gmail.com>
2020-01-26 22:32:09 -0500

    add slot machine exercise

---
+++ 03_Props_and_more/slot_machine/index.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Slot Machines!</h1>
+                <Machine s1='🍊' s2='🍓' s3='🍌' />
+                <Machine s1='🍓' s2='🍓' s3='🍓' />
+                <Machine s1='🍌' s2='🍓' s3='🍌' />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
