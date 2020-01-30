9f1440b843c6384b61e8574afda6b4b7564f3d35
Marc Turner <marc.t1967@gmail.com>
2020-01-29 22:32:37 -0500

    add done module exercise

--- Create_React_App/Module_Excercise/module-excercise/src/index.js
+++ Create_React_App/Module_Excercise/module-excercise/src/index.js
@@ -9,4 +9,7 @@ console.log(`I'd like one ${fruit} please.`);
 console.log(`Here you go: ${fruit}`);
 // log message "Delicious may I have another"
 console.log(`Delicious! May I have another?`);
+// Remove the fruit from the array of fruits
+let remaining = remove(foods, fruit);
 // Log message "sorry, we're all out We have fuitsleft left"
+console.log(`I'm sorry, we're all out. We have ${remaining.length}others left`);
